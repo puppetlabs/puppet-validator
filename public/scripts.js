5a2b4cd76d1937c5c533e15e97e37877a7d698f5
Ben Ford <ben.ford@puppetlabs.com>
2016-02-11 16:30:32 -0800

    use tab in the textarea

--- public/scripts.js
+++ public/scripts.js
@@ -11,4 +11,27 @@ function toggleMenu() {
 
 $( document ).ready(function() {
   toggleChecks();
+
+  $("textarea#code").keydown(function(e) {
+      if(e.keyCode === 9) { // tab was pressed
+          // get caret position/selection
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+
+          var $this = $(this);
+          var value = $this.val();
+
+          // set textarea value to: text before caret + tab + text after caret
+          $this.val(value.substring(0, start)
+                      + "\t"
+                      + value.substring(end));
+
+          // put caret at right position again (add one for the tab)
+          this.selectionStart = this.selectionEnd = start + 1;
+
+          // prevent the loss of focus
+          e.preventDefault();
+      }
+  });
+
 });
